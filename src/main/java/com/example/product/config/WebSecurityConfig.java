a9fcf9aee38412ca8d16a61dd34db521a6f53375
ibo415 <ibrahim_97_o@hotmail.de>
2024-12-10 22:35:25 +0100

    authentication provider

--- src/main/java/com/example/product/config/WebSecurityConfig.java
+++ src/main/java/com/example/product/config/WebSecurityConfig.java
@@ -1,15 +1,23 @@
 package com.example.product.config;
 
 
+import com.example.product.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
+    @Autowired
+    MyUserDetailsService myUserDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
@@ -21,4 +29,20 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(myUserDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+
+        return provider;
+    }
+
+
 }
