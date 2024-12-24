bee411dd3dc27a2e7c3b7c603727a1998825f9ae
ibo415 <ibrahim_97_o@hotmail.de>
2024-12-24 23:15:14 +0100

    endpoint with number of products edited

--- src/main/java/com/example/product/controller/CartController.java
+++ src/main/java/com/example/product/controller/CartController.java
@@ -1,6 +1,7 @@
 package com.example.product.controller;
 
 import com.example.product.model.Cart;
+import com.example.product.model.CartProduct;
 import com.example.product.model.Product;
 import com.example.product.service.CartService;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +19,16 @@ public class CartController {
     }
 
     @GetMapping("/{userId}")
-    public List<Product> getCartProducts(@PathVariable Long userId) {
+    public List<CartProduct> getCartProducts(@PathVariable Long userId) {
         Cart cart = cartService.getCartByUserId(userId);
 
-        return cart.getProducts();
+        return cart.getCartProducts();
     }
 
 
-    @PostMapping("/{userId}/add/{productId}")
-    public Cart addProductToCart(@PathVariable Long userId, @PathVariable Long productId){
-        return cartService.addProductToCart(userId,productId);
+    @PostMapping("/{userId}/add/{productId}/{quantity}")
+    public Cart addProductToCart(@PathVariable Long userId, @PathVariable Long productId, @PathVariable int quantity){
+        return cartService.addProductToCart(userId,productId, quantity);
 
     }
 
